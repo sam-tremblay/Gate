4d6b9377b86701aaddf7919b21737c0142702cf2
Samuel Tremblay-Meunier <samuel.t@deuxparquatre.com>
2021-11-07 16:31:28 -0500

    Move ACF JSON Datas

--- final/frame/modules/gate/class/datas.php
+++ final/frame/modules/gate/class/datas.php
@@ -2,10 +2,12 @@
 
 	class gateManageDatas{
 
-
-		static $acf_path = __DIR__ . '/../datas/acf';
+		public $acf_path;
 
 		function __construct(){
+
+			$this->acf_path = get_stylesheet_directory() . '/datas/acf';
+
 			if(class_exists('acf'))
 				$this->acf();
 		}
@@ -14,7 +16,7 @@
 		function acf(){
 
 			add_filter('acf/settings/save_json', function($path){
-				return gateManageDatas::$acf_path;
+				return $this->acf_path;
 			});
 
 			add_filter('acf/settings/load_json', function($paths){
@@ -22,7 +24,7 @@
 				unset( $paths[0] );
 
 				// Append our new path
-				$paths[] = gateManageDatas::$acf_path;
+				$paths[] = $this->acf_path;
 
 				return $paths;
 			});
