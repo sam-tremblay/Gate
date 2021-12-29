655aafb9e3c023ee8b689dcc78b25ec578ad1f01
Samuel Tremblay-Meunier <samuel.t@deuxparquatre.com>
2021-12-29 18:13:43 -0500

    Add Role Manager

--- final/frame/modules/gate/class/roles.php
+++ final/frame/modules/gate/class/roles.php
@@ -5,12 +5,48 @@
 		function __construct(){
 
 			/*
-			* Init
+			* ACF Init
 			*/
-			add_action('init', function(){
-				// add roles
-				$this->add();
-			});
+			if(class_exists('acf')){
+				add_action('acf/init', function(){
+					// add Role Management
+					$this->manager();
+
+					// Add option in Role Management
+					$this->acf();
+
+					// Add Roles created from Role Management
+					$this->add();
+				});
+			}
+
+		}
+
+
+		function manager(){
+			$labels = array(
+				'name'                     => __( 'Roles', 'gate' ),
+		        'singular_name'            => __( 'Role', 'gate' )
+			);
+			$args  = array(
+				'labels'             => $labels,
+				'description'		 => '',
+		        'public'             => false,
+		        'publicly_queryable' => false,
+		        'show_ui'            => true,
+		        'show_in_menu'       => false,
+		        'show_in_nav_menus'  => false,
+		        'query_var'          => false,
+		        'capability_type'    => 'post',
+		        'has_archive'        => false,
+		        'hierarchical'       => false,
+		        'menu_position'      => null,
+		        'supports'           => array('title'),
+			);
+			register_post_type("gate_role", $args);
+		}
+
+		function acf(){
 
 		}
 
