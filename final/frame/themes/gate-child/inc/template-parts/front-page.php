1ab34ed16098612eebffde8f060cc84fe6a12324
Samuel Tremblay-Meunier <samuel.t@deuxparquatre.com>
2022-02-04 03:33:59 -0500

    Remove Front-end Test

--- final/frame/themes/gate-child/inc/template-parts/front-page.php
+++ final/frame/themes/gate-child/inc/template-parts/front-page.php
@@ -1,5 +1,1 @@
 <?php ?>
-<div id="pageWrap">
-<div style="background: #ff0000; height: 100vh;"></div>
-<div style="background: #00ff00; height: 100vh;"></div>
-</div>
--- workspace/themes/gate-child/js/frontend/init.js
+++
@@ -1,1 +0,0 @@
-$('#pageWrap').gateScroll();
