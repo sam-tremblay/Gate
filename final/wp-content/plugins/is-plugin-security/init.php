d5129c5549c865f9a5b3a6d854f05098f2d35ca3
Samuel Tremblay-Meunier <samuel.t@deuxparquatre.com>
2022-05-14 21:50:59 -0400

    Update Security Plugin

--- final/wp-content/plugins/is-plugin-security/init.php
+++ final/wp-content/plugins/is-plugin-security/init.php
@@ -92,6 +92,49 @@ if (!class_exists('gateSecurity')){
 			if(!wp_next_scheduled('requesters_list_updater'))
 				wp_schedule_event(time(), 'hourly', 'requesters_list_updater');
 
+
+
+			/*
+			* On plugin activation
+			*/
+			register_activation_hook(__FILE__, function(){
+
+				/*
+				* Active Core Plugin
+				*/
+				if(file_exists(WP_PLUGIN_DIR . '/is-gate-core/init.php') && !is_plugin_active('is-gate-core/init.php'))
+					activate_plugin('is-gate-core/init.php');
+				elseif(!file_exists(WP_PLUGIN_DIR . '/is-gate-core/init.php')){
+
+					/*
+					* If Core Plugin not There
+					*/
+
+					wp_die(
+						__('L\'extension "Gate Core" est manquante.', 'is-gate-seo'),
+						__('Une erreur est survenue', 'is-gate-seo'),
+						[
+							'back_link' => true
+						]
+					);
+
+				}
+
+
+			});
+
+
+			add_action('admin_init', function(){
+
+				/*
+				* If "Gate" is deactivated
+				*/
+				if(!file_exists(WP_PLUGIN_DIR . '/is-gate-core/init.php') || !is_plugin_active('is-gate-core/init.php'))
+					deactivate_plugins(plugin_basename(__FILE__));
+
+				
+			});
+
 		}
 
 
